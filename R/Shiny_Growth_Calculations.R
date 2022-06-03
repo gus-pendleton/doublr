3a539c42a2c92439d3433c523fdaf59d19a2518e
Augustus Pendleton <guspendleton@gmail.com>
2022-06-03 10:45:22 -0400

    Added documentation to double_times() function

--- R/Shiny_Growth_Calculations.R
+++ R/Shiny_Growth_Calculations.R
@@ -1,5 +1,17 @@
 #' Calculate doubling time in exponential phase
 #' @export
+#'
+#' @param df A dataframe
+#' @param time The column holding numerical or datetime values for the independent (usually time) variable. Quotation marks necessary
+#' @param measure The column holding numerical values for the dependent (e.g. OD) variable, not log-transformed. Quotation marks necessary.
+#' @param c1,c2  Cutoffs for the time column that bound the exponential phase of the data.
+#'
+#' @return A doubling time produced by fitting a linear model between the log2-transformed measure column and the time column and taking the reciprocal of the slope from that model.
+#'
+#' @examples
+#' #Make simple dataframe with exponential data:
+#' df<-data.frame(x= c(1:10), y = 2^c(1:10))
+#' double_times(df, time = x, measure = y, c1 = 1, c2 = 8)
 double_times<-function(df,time, measure, c1,c2){
   exp<-df%>%
     filter(!!as.symbol(time)>=c1, !!as.symbol(time)<=c2)%>%
